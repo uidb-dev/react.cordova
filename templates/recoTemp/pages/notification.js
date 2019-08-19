e0e080512ee790086b13887a9b801316ed4f18ea
orchoban <orchoban@gmail.com>
2019-08-19 23:58:38 +0300

    20190819.01

--- templates/recoTemp/pages/notification.js
+++ templates/recoTemp/pages/notification.js
@@ -1,26 +1,58 @@
 import React from 'react';
+import BrowserNotifications from 'react-browser-notifications';
 
 export default class Notification extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            notificationTitle: "",
+            notificationText: "",
+        }
+
+    }
 
     notification() {
 
         const this_ = this;
 
-        navigator.notification.confirm(
-            'You are the winner!', // message
-            this_.onConfirm,            // callback to invoke with index of button pressed
-            'Game Over',           // title
-            ['Restart', 'Exit']     // buttonLabels
-        );
-    }
+        //on mobile platform
+        if (window.cordova)
+            if (window.cordova.platformId !== "browser") {
+                //-- https://github.com/katzer/cordova-plugin-local-notifications.git  --//
+                window.cordova.plugins.notification.local.schedule({
+                    title: this_.state.notificationTitle,
+                    text: this_.state.notificationText,
+                });
+                return;
+            }
 
-    onConfirm(buttonIndex) {
-        alert('You selected button ' + buttonIndex);
+        //om browser platform
+        if (this.browserNotifications.supported()) this.browserNotifications.show();
+
     }
 
     render() {
+        const this_ = this;
         return <div>
-            <h1 onClick={() => this.notification()}>Notification</h1>
+            <h2>title</h2>  <input onKeyUp={(e) => { this_.setState({ notificationTitle: e.currentTarget.value }); }} type="text" />
+            <h2>text</h2>  <input onKeyUp={(e) => { this_.setState({ notificationText: e.currentTarget.value }); }} type="text" />
+            <br />
+            <br />
+            <button onClick={() => this.notification()}>Notification</button>
+
+<br/>
+<p>* it's only work from <span>reco start</span>(local server on localhost), <span>mobile device</span> or <span>https url</span></p>
+
+            {/* https://www.npmjs.com/package/react-browser-notifications */}
+            <BrowserNotifications
+                onRef={ref => (this_.browserNotifications = ref)} // Required
+                title={this_.state.notificationTitle} // Required
+                body={this_.state.notificationText}
+                icon="./favicon.ico"
+                timeout="3000"
+                onClick={event => console.log(event)}
+            />
         </div>
     }
 }
