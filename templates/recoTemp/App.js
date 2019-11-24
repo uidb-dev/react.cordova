d950797130696a9762e38e8cef0fe5149a950c49
orchoban <33927385+orchoban@users.noreply.github.com>
2019-11-24 11:45:49 +0200

    Learn more about the Navigator

--- templates/recoTemp/App.js
+++ templates/recoTemp/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigator from 'react.cordova-navigation_controller';
+// Learn more about the Navigator: https://www.npmjs.com/package/react.cordova-navigation_controller
 
 import './app.css';
 
